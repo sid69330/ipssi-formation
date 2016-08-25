673a1f2bb8cf2e65865495510da87de2748c4b1e
Alexandre <alexandre.julien.91@gmail.com>
2016-08-25 16:35:59 +0200

    début mot de passe oublié + envoi mail

--- ipssi-ipformation/application/controllers/Connexion.php
+++ ipssi-ipformation/application/controllers/Connexion.php
@@ -16,7 +16,7 @@ class Connexion extends CI_Controller
 	
 	public function index()
 	{
-		$this->form_validation->set_rules('identifiant', '"Identifiant"', 'trim|required|valid_email|encode_php_tags');
+		$this->form_validation->set_rules('identifiant', '"Email"', 'trim|required|valid_email|encode_php_tags');
 		$this->form_validation->set_rules('mdp', '"Mot de passe"', 'required|encode_php_tags');
 
 		$menu['title'] = "Connexion à l'intranet";
@@ -60,7 +60,66 @@ class Connexion extends CI_Controller
 			$this->load->view('include/menu.php', $menu);
 			$this->load->view('connexion/connexion.php');
 		}
-	}	
+	}
+
+	public function mot_de_passe_oublie()
+	{
+		$this->form_validation->set_rules('identifiant', '"Email"', 'trim|required|valid_email|is_exist[utilisateur.mail_utilisateur]|encode_php_tags');
+
+		if($this->form_validation->run() == FALSE)
+		{
+			$menu['title'] = "Connexion à l'intranet";
+			$menu['menu'] = $this->menu->recupMenu();
+
+			$data['success'] = $this->session->flashdata('success');
+
+			$this->load->view('include/menu.php', $menu);
+			$this->load->view('connexion/mot-de-passe-oublie.php', $data);
+		}
+		else
+		{
+			$email = $this->input->post('identifiant');
+
+			$characts = 'abcdefghijklmnopqrstuvwxyz';
+			$characts .= 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';	
+			$characts .= '1234567890'; 
+			$code_aleatoire = ''; 
+
+			for($i=0; $i < 100; $i++)
+			{ 
+				$code_aleatoire .= substr($characts, rand()%(strlen($characts)),1); 
+			}
+
+			$this->db->trans_start();
+
+			$ok = $this->connexion_model->genererCleMdp($email, $code_aleatoire);
+
+			if($ok)
+			{
+				$this->db->trans_complete();
+				$this->load->library('envoi_mail');
+
+				$body = 'Bonjour, <br/><br/>';
+				$body .= 'Pour modifier votre mot de passe merci de cliquer ici : <a href="'.site_url('/connexion/modifier_mdp/'.$code_aleatoire).'">Réinitialiser mon mot de passe</a><br/><br/>';
+				$body .= 'L\'équipe IPSSI';
+				$this->envoi_mail->envoyer_email($email, 'Réinitialisation du mot de passe', $body);
+
+				$this->session->set_flashdata('success', 'Un email vous a été envoyé contenant un lien permettant de réinitialiser votre mot de passe.');
+                Redirect('/connexion/mot_de_passe_oublie');
+			}
+			else
+			{
+				$this->db->trans_rollback();
+
+				$menu['title'] = "Connexion à l'intranet";
+				$menu['menu'] = $this->menu->recupMenu();
+				$data['erreur'] = 'Une erreur s\'est produite pendant la mise à jour de votre compte';
+
+				$this->load->view('include/menu.php', $menu);
+				$this->load->view('connexion/mot-de-passe-oublie.php', $data);
+			}
+		}
+	}
 }
 
 ?>
