86af6e6c96822c786ac4769fb8431b7d8841bfc2
Romane MOSSON <romane.mosson@gmail.com>
2016-08-26 15:42:20 +0200

    Ajouter + modifier + début valider NOte de frais

--- ipssi-ipformation/application/controllers/back/Ressources_humaines_back.php
+++ ipssi-ipformation/application/controllers/back/Ressources_humaines_back.php
@@ -63,14 +63,13 @@ class Ressources_humaines_back extends MY_Controller
         if(($idNoteFrais == '') || (!$this->ressources_humaines_back_model->noteFraisExiste($idNoteFrais)))
             Redirect('/ipssi/ressources-humaines/note-frais');
 
-        // droit suffisant en Lecture
-
-        $menu['title'] = "IPSSI - Note de frais - Détail";
+        $menu['title'] = "IPSSI - Note de frais - Supprimer";
         $menu['back'] = $this->back;
         $menu['menu'] = $this->menu->recupMenuBack($this->session->userdata('id'));
         $data['droits'] = $this->droits;
 
         $ok = $this->ressources_humaines_back_model->supprimerNoteFrais($idNoteFrais, $this->session->userdata('id'), $this->droits);
+
         if($ok)
             $this->session->set_flashdata('success', 'Note de Frais supprimée avec succès.');
         else
@@ -79,38 +78,191 @@ class Ressources_humaines_back extends MY_Controller
         Redirect('/ipssi/ressources-humaines/note-frais');
     }
 
+
     public function modifier_note_frais($idNoteFrais = '')
     {
         if(($idNoteFrais == '') || (!$this->ressources_humaines_back_model->noteFraisExiste($idNoteFrais)))
             Redirect('/ipssi/ressources-humaines/note-frais');
 
-        $menu['title'] = "IPSSI - Note de frais - Détail";
-        $menu['back'] = $this->back;
-        $menu['menu'] = $this->menu->recupMenuBack($this->session->userdata('id'));
-        $data['droits'] = $this->droits;
+        $data['note_frais'] = $this->ressources_humaines_back_model->recupNoteFraisAModifier($idNoteFrais, $this->session->userdata('id'), $this->droits);
+        $data['type_note_frais'] = $this->ressources_humaines_back_model->recupTypeNoteFrais();
+
+
+        if($this->droit->droitSuffisantModifier($this->droits, $data['note_frais']->id_utilisateur, $this->session->userdata('id')))
+        {
+            $this->form_validation->set_rules('type_note_frais', '"Type de la note"', 'required');
+            $this->form_validation->set_rules('description', '"Description"', 'trim|required|encode_php_tags');
+            $this->form_validation->set_rules('date_note', '"Date note de Frais"', 'required|date|encode_php_tags');
+            $this->form_validation->set_rules('montant', '"Montant de la note"', 'required|encode_php_tags');
+
+            
+            if($this->form_validation->run() == FALSE)
+            {
+
+                $menu['title'] = "IPSSI - Note de frais - Modifier";
+                $menu['back'] = $this->back;
+                $menu['menu'] = $this->menu->recupMenuBack($this->session->userdata('id'));
+               
+                $data['success'] = $this->session->flashdata('success');
+                $data['droits'] = $this->droits;
+
+                $this->load->view('back/include/menu.php', $menu);
+                $this->load->view('back/ressources_humaines/modifier-notes-frais.php', $data);
+            }
+            else
+            {
+                $erreur = '';
+
+                $type_note_frais = $this->input->post('type_note_frais');
+                $description = $this->input->post('description');
+                $date_note = $this->input->post('date_note');
+                $montant = $this->input->post('montant');
+                $trajet = $this->input->post('trajet');
+                $km_parcouru = $this->input->post('km_parcouru');
+                $prix_km = $this->input->post('prix_km');
+
+                if($erreur != '')
+                {
+                    $menu['title'] = "IPSSI - Note de frais - Modifier";
+                    $menu['back'] = $this->back;
+                    $menu['menu'] = $this->menu->recupMenuBack($this->session->userdata('id'));
+                    $data['erreur'] = $erreur;
+
+                    $this->load->view('back/include/menu.php', $menu);
+                    $this->load->view('back/ressources_humaines/modifier-notes-frais.php', $data);
+                }
+                else
+                {                  
+                    $this->ressources_humaines_back_model->modifier_note_frais($type_note_frais, $description, $date_note, $montant, $trajet, $km_parcouru, $prix_km, $data['note_frais']->id_note_frais);
+                    $this->session->set_flashdata('success', 'Note de Frais modifiée avec succès.');
+                        Redirect('/ipssi/ressources-humaines/note-frais/modifier/'.$data['note_frais']->id_note_frais);
+                }
+
+            }
+        }
+        else
+            Redirect('/ipssi/ressources-humaines/note-frais');
     }
 
     public function ajouter_note_frais()
     {
-        $menu['title'] = "IPSSI - Note de frais - Détail";
+        $menu['title'] = "IPSSI - Note de frais - Ajouter";
         $menu['back'] = $this->back;
         $menu['menu'] = $this->menu->recupMenuBack($this->session->userdata('id'));
+       
         $data['droits'] = $this->droits;
         $data['type_note_frais'] = $this->ressources_humaines_back_model->recupTypeNoteFrais();
 
-        $this->load->view('back/include/menu.php', $menu);
-        $this->load->view('back/ressources_humaines/ajouter-notes-frais.php', $data);
+        $this->form_validation->set_rules('type_note_frais', '"Type de la note"', 'required');
+        $this->form_validation->set_rules('description', '"Description"', 'trim|required|encode_php_tags');
+        $this->form_validation->set_rules('date_note', '"Date note de Frais"', 'required|date|encode_php_tags');
+        $this->form_validation->set_rules('montant', '"Montant de la note"', 'required|encode_php_tags');
+
+        if($this->form_validation->run() == FALSE)
+        {
+            $data['success'] = $this->session->flashdata('success');
+            $this->load->view('back/include/menu.php', $menu);
+            $this->load->view('back/ressources_humaines/ajouter-notes-frais.php', $data);
+        }
+        else
+        {
+            if(isset($erreur))
+            {
+                $data['erreur'] = $erreur;
+                $this->load->view('back/include/menu.php', $menu);
+                $this->load->view('back/ressources_humaines/ajouter-notes-frais.php', $data);
+            }
+            else
+            {
+
+                $erreur = '';
+
+                $type_note_frais = $this->input->post('type_note_frais');
+                $description = $this->input->post('description');
+                $date_note = $this->input->post('date_note');
+                $montant = $this->input->post('montant');
+                $trajet = $this->input->post('trajet');
+                $km_parcouru = $this->input->post('km_parcouru');
+                $prix_km = $this->input->post('prix_km');
+
+                $id_insere = $this->ressources_humaines_back_model->ajouter_note_frais($type_note_frais, $description, $date_note, $montant, $trajet, $km_parcouru, $prix_km, $this->session->userdata('id'));
+                if($id_insere != '')
+                {
+                    $this->session->set_flashdata('success', 'Note de frais ajoutée avec succès.');
+                        Redirect('/ipssi/ressources-humaines/note-frais/ajouter');    
+                }
+                else
+                {
+                    $data['erreur'] = 'Aucune modification effectuée.';
+                    $this->load->view('back/include/menu.php', $menu);
+                    $this->load->view('back/ressources_humaines/ajouter-notes-frais.php', $data);
+                }                
+            }
+        }
     }
 
     public function valider_note_frais($idNoteFrais = '')
     {
+
+        //Validation N+1 et/ou RH
+        //Ajouter les champs Type de paiement + date de paiement 
+        // Si etat <> 3 => motif_refus = null
+
+
         if(($idNoteFrais == '') || (!$this->ressources_humaines_back_model->noteFraisExiste($idNoteFrais)))
             Redirect('/ipssi/ressources-humaines/note-frais');
 
-        $menu['title'] = "IPSSI - Note de frais - Détail";
-        $menu['back'] = $this->back;
-        $menu['menu'] = $this->menu->recupMenuBack($this->session->userdata('id'));
-        $data['droits'] = $this->droits;
+        $data['note_frais'] = $this->ressources_humaines_back_model->recupNoteFraisAModifier($idNoteFrais, $this->session->userdata('id'), $this->droits);
+        $data['etat_note_frais'] = $this->ressources_humaines_back_model->recupEtatNoteFrais();
+
+        if($this->droit->droitSuffisantModifier($this->droits, $data['note_frais']->id_utilisateur, $this->session->userdata('id')))
+        {
+            $this->form_validation->set_rules('etat_note_frais', '"Etat de la note"', 'required');
+            if($this->input->post('etat_note_frais') == 3)
+                $this->form_validation->set_rules('motif_refus', '"Motif du refus"', 'required');
+            
+
+            if($this->form_validation->run() == FALSE)
+            {
+
+                $menu['title'] = "IPSSI - Note de frais - Valider";
+                $menu['back'] = $this->back;
+                $menu['menu'] = $this->menu->recupMenuBack($this->session->userdata('id'));
+               
+                $data['success'] = $this->session->flashdata('success');
+                $data['droits'] = $this->droits;
+
+                $this->load->view('back/include/menu.php', $menu);
+                $this->load->view('back/ressources_humaines/valider-notes-frais.php', $data);
+            }
+            else
+            {
+                $erreur = '';
+
+                $etat_note_frais = $this->input->post('etat_note_frais');
+                $motif_refus = $this->input->post('motif_refus');
+
+                if($erreur != '')
+                {
+                    $menu['title'] = "IPSSI - Note de frais - Valider";
+                    $menu['back'] = $this->back;
+                    $menu['menu'] = $this->menu->recupMenuBack($this->session->userdata('id'));
+                    $data['erreur'] = $erreur;
+
+                    $this->load->view('back/include/menu.php', $menu);
+                    $this->load->view('back/ressources_humaines/valider-notes-frais.php', $data);
+                }
+                else
+                {                  
+                    $this->ressources_humaines_back_model->valider_note_frais($data['note_frais']->id_note_frais,$etat_note_frais, $smotif_refus);
+                    $this->session->set_flashdata('success', 'Note de Frais modifiée avec succès.');
+                        Redirect('/ipssi/ressources-humaines/note-frais/valider/'.$data['note_frais']->id_note_frais);
+                }
+
+            }
+        }
+        else
+            Redirect('/ipssi/ressources-humaines/note-frais');
     }
 
     /* --------------------------------------- */
